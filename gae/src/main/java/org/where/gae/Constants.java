69366f35ef0e3fb3f44073fe8dc47cf35d7c6193
mth08 <matthias.thoma@gmail.com>
2014-06-29 14:29:47 +0200

    merge

--- gae/src/main/java/org/where/gae/Constants.java
+++ gae/src/main/java/org/where/gae/Constants.java
@@ -6,9 +6,7 @@ package org.example.where.gae;
 public class Constants {
     public static final String WEB_LOCALHOST_CLIENT_ID = "649196046194-3klk1n94lka1g0dkn6aeupvuvhlb85h0.apps.googleusercontent.com";
     public static final String WEB_APPSPOT_CLIENT_ID = "649196046194-qd83ij5jluu9s126s0vai6qf9pgm82fo.apps.googleusercontent.com";
-    public static final String ANDROID_CLIENT_ID = "649196046194-390pn70flkt2m1h55t44q87v0qf95is9.apps.googleusercontent.com";
-    public static final String ANDROID_MATTHIAS = "649196046194-191pkktnbgroq2moorg25eej0qvstvcs.apps.googleusercontent.com";
-    public static final String ANDROID_WHERE = "649196046194-h7l5djpol57uv9a6juai6ih5f852lqgp.apps.googleusercontent.com";
+    public static final String ANDROID_WHERE = "649196046194-4ljonvo2p2it857p9bh1i1diqg6l5eae.apps.googleusercontent.com";
     public static final String IOS_CLIENT_ID = "----- NONE ------";
     public static final String ANDROID_AUDIENCE = WEB_APPSPOT_CLIENT_ID;
     public static final String API_EXPLORER_CLIENT_ID = com.google.api.server.spi.Constant.API_EXPLORER_CLIENT_ID;
--- gae/src/main/java/org/where/gae/WhereService.java
+++ gae/src/main/java/org/where/gae/WhereService.java
@@ -1,278 +1,167 @@
-package org.example.where.gae;
+package org.example.where.service;
 
-import com.google.android.gcm.server.Message;
-import com.google.android.gcm.server.Result;
-import com.google.android.gcm.server.Sender;
-import com.google.api.server.spi.ServiceException;
-import com.google.api.server.spi.config.Api;
-import com.google.api.server.spi.config.ApiMethod;
-import com.google.appengine.api.users.User;
-import com.googlecode.objectify.ObjectifyService;
+import android.app.Service;
+import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Message;
+import android.util.Log;
 
-import java.io.IOException;
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.ConcurrentSkipListSet;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import org.example.where.broadcast.GcmBroadcastReceiver;
+import org.example.where.activity.GooglePlayServicesActivity;
+import org.example.where.broadcast.WearBroadcastReceiver;
 
-import javax.inject.Named;
+import org.example.where.application.WhereApplication;
+import org.example.where.util.WearHelper;
+import org.example.where.util.WhereHelper;
+import org.example.where.util.GetAddressTask;
 
-import static com.googlecode.objectify.ObjectifyService.ofy;
+public class WhereService extends Service {
+    public static final String TAG = WhereService.class.getName();
 
-/**
- * Defines v1 of a 'where' API, which provides simple "whereIs" method.
- */
-@Api(
-        name = "where",
-        version = "v1",
-        scopes = {Constants.EMAIL_SCOPE},
-        clientIds = {Constants.WEB_LOCALHOST_CLIENT_ID,  // localhost test
-                Constants.WEB_APPSPOT_CLIENT_ID,         // https://unique-voyage-610.appspot.com/
-                Constants.ANDROID_CLIENT_ID,             // Florian's debug key
-                Constants.ANDROID_MATTHIAS,              // Matthias's debug key
-                Constants.ANDROID_WHERE,                 // where-certificate debug
-                Constants.IOS_CLIENT_ID,                 // not set
-                Constants.API_EXPLORER_CLIENT_ID},       // Google Explorer API
-        audiences = {Constants.ANDROID_AUDIENCE}
-)
-public class WhereService {
-    /* initialize Objectify */
-    static {
-        ObjectifyService.register(GcmReqistration.class);
+    public static final String KEY_ACTION = "action";
+    public static final String KEY_REQUESTER = "requester";
+    public static final String KEY_NAME = "name";
+    public static final String LOCATE = "locate";
+    public static final String DISCLOSE = "disclose";
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return new WhereBinder(this);
     }
 
-    private static final Logger logger = Logger.getLogger(WhereService.class.getName());
+    @Override
+    public void onCreate() {
+        super.onCreate();
 
-    public static final int UNKNOWN_USER = 1;
-    public static final int INACTIVE_USER = 2;
-    public static final int CANNOT_SEND_GCM_MESSAGE = 3;
+        Log.i(TAG, "Service created.");
 
-    private static WhereService instance;
-
-    //private ConcurrentMap<String, String> gcmRegistrations = new ConcurrentHashMap<>();
-    private ConcurrentMap<String, Set<String>> locationRequestsMap = new ConcurrentHashMap<>();
-    private Set<String> activeUsers = new ConcurrentSkipListSet<>();
-
-    public WhereService() {
-        instance = this;
+        // Creates an initial notification. In a better Version of the app this should be an
+        // always on command, so that you can initiate this from Android Wear without the need
+        // of an Notification
+        WhereApplication.getContext().getWearHelper().setupWear();
     }
 
-    @ApiMethod(name = "register", httpMethod = "post")
-    public WhereStatus registerDevice(User user, @Named("gcm_registration_id") String gcmRegistrationId) throws ServiceException {
-        logger.info("GCM registration of user: " + user);
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG, "Service started.");
+        WhereApplication.getContext().getWearHelper().registerReceiver(this);
 
-        if (user == null) {
-            throw new ServiceException(401, new NullPointerException("user"));
+        if (intent != null) {
+            onHandleIntent(intent);
         }
 
-        if (gcmRegistrationId == null) {
-            throw new ServiceException(400, new NullPointerException("gcm_registration_id"));
-        }
-
-        String email = formatEmail(user.getEmail());
-
-        logger.info("user " + email + " has registered.");
-
-//        gcmRegistrations.put(user.getEmail(), gcmRegistrationId);
-        ofy().save().entity(new GcmReqistration(email, gcmRegistrationId)).now();
-
-        return new WhereStatus("registration is ok");
+        return START_STICKY;
     }
 
-    @ApiMethod(name = "locate", httpMethod = "post")
-    public WhereStatus requestLocation(User user, @Named("email") String email) throws ServiceException {
-        logger.info("user: " + user + " email: " + email);
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
 
-        if (user == null) {
-            throw new ServiceException(401, new NullPointerException("user"));
-        }
+        WhereApplication.getContext().getWearHelper().cancelNotification();
 
-        if (email == null) {
-            throw new ServiceException(400, new NullPointerException("email"));
-        }
-
-        String requesterEmail = formatEmail(user.getEmail());
-        email = formatEmail(email);
-
-//        String gcmRegiId = gcmRegistrations.get(email);
-        GcmReqistration gcmReqistration = ofy().load().type(GcmReqistration.class).id(email).now();
-
-        if (gcmReqistration == null) {
-            // user is not known
-            return new WhereStatus(UNKNOWN_USER, email + " is unknown");
-        } else {
-            if (!activeUsers.contains(email)) {
-                logger.info(email + " is NOT active");
-                return new WhereStatus(INACTIVE_USER, email + " is NOT active right now");
-            }
-
-            String gcmRegiId = gcmReqistration.gcmId;
-
-            try {
-                logger.info("Sending GCM message to " + email + " from " + requesterEmail);
-                Map<String, String> message = new HashMap<>();
-                message.put("requester", requesterEmail);
-                message.put("name", user.getNickname());
-                message.put("action", "locate");
-                sendMessageToDevice(gcmRegiId, message);
-
-                /* enqueue current user's request */
-                locationRequestsMap.putIfAbsent(email, new ConcurrentSkipListSet<String>());
-                locationRequestsMap.get(email).add(requesterEmail);
-            } catch (IOException e) {
-                logger.log(Level.WARNING, "cannot send GCM message", e);
-                return new WhereStatus(CANNOT_SEND_GCM_MESSAGE, "cannot send message to " + email);
-            }
-
-
-            return new WhereStatus("accepted");
-        }
+        Log.i(TAG, "Service destroyed.");
     }
 
-    @ApiMethod(name = "isRegistered", httpMethod = "post")
-    public WhereStatus isRegistered(User user, @Named("email") String email) throws ServiceException {
-        logger.info("user: " + user + " email: " + email);
+    protected void onHandleIntent(Intent intent) {
+        Bundle extras = intent.getExtras();
+        String actionStr = intent.getAction();
 
-        if (user == null) {
-            throw new ServiceException(401, new NullPointerException("user"));
+        // Hack to work with the intents coming from Wear. A real software engineeering approach
+        // would look different, but it is a Hackaton :-)
+
+        if (actionStr != null)
+        if (actionStr.equals(WearHelper.ACTION_RESPONSE)) {
+            WhereApplication.getContext().getWearHelper().processWearResponse(intent);
+            WearBroadcastReceiver.completeWakefulIntent(intent);
+            return ;
         }
 
-        if (email == null) {
-            throw new ServiceException(400, new NullPointerException("email"));
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+        if (gcm == null) {
+            return; //nothing to do
         }
 
-        String requesterEmail = formatEmail(user.getEmail());
-        email = formatEmail(email);
+        if (extras != null && !extras.isEmpty()) {  // has effect of unparcelling Bundle
+            // The getMessageType() intent parameter must be the intent you received
+            // in your BroadcastReceiver.
+            String messageType = gcm.getMessageType(intent);
 
-        GcmReqistration gcmReqistration = ofy().load().type(GcmReqistration.class).id(email).now();
+            Log.i(TAG, "Received GCM message: " + extras);
 
-        if (gcmReqistration == null) {
-            logger.info("user is NOT registered.");
-            return new WhereStatus(UNKNOWN_USER, "unknown user: " + email);
-        } else {
-            boolean isActive = activeUsers.contains(email);
+            /*
+             * Filter messages based on message type. Since it is likely that GCM
+             * will be extended in the future with new message types, just ignore
+             * any message types you're not interested in, or that you don't
+             * recognize.
+             */
+            if (GoogleCloudMessaging.
+                    MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
+                WhereHelper.setTextStatus("Send error: " + extras.toString());
+            } else if (GoogleCloudMessaging.
+                    MESSAGE_TYPE_DELETED.equals(messageType)) {
+                WhereHelper.setTextStatus("Deleted messages on server: " + extras.toString());
+                // If it's a regular GCM message, do some work.
+            } else if (GoogleCloudMessaging.
+                    MESSAGE_TYPE_MESSAGE.equals(messageType)) {
+                if (extras.containsKey(KEY_ACTION)) {
+                    String action = extras.getString(KEY_ACTION);
+                    String requester = extras.getString(KEY_REQUESTER);
+                    final String name = extras.getString(KEY_NAME);
 
-            String status = "'" + email + "' is registered and " + (isActive ? "" : "in") + "active";
-            logger.info(status);
-            return new WhereStatus(status);
-        }
-    }
+                    if (LOCATE.equals(action) && extras.containsKey(KEY_REQUESTER)) {
+                        Log.i(TAG, "Determining current location");
 
-    @ApiMethod(name = "setActiveFlag", httpMethod = "post")
-    public WhereStatus setActiveFlag(User user, @Named("active") boolean active) throws ServiceException {
-        logger.info("user: " + user + " active: " + active);
+                        // inform about the location request
+                        Message msg = Message.obtain();
+                        msg.what = GooglePlayServicesActivity.LOCATION_REQUEST;
+                        msg.obj = requester;
+                        WhereHelper.sendMessage(msg);
 
-        if (user == null) {
-            throw new ServiceException(401, new NullPointerException("user"));
-        }
+                        //todo: check if user is allowed to receive location
 
-        String requesterEmail = formatEmail(user.getEmail());
+                        if (WhereApplication.getContext().isActive()) {
+                            //update notification with the location request
 
-        GcmReqistration gcmReqistration = ofy().load().type(GcmReqistration.class).id(requesterEmail).now();
+                            final Location location = WhereApplication.getContext().getLastLocation();
+                            Log.i(TAG, "Current location: " + location);
+                            if (location != null) {
+                                new GetAddressTask(WhereApplication.getContext()){
+                                    @Override
+                                    protected void onPostExecute(String s) {
+                                        WhereHelper.sendLocation(name, s);
+                                    }
+                                }.execute(location);
+                            }
+                        } else {
+                            Log.i(TAG, "User is NOT active, will not reveal his/her location");
+                            WhereHelper.sendLocation(name, "unavailable");
+                        }
+                    } else if (DISCLOSE.equals(action)) {
+                                                Log.i(TAG, "Location received from GCM: " + extras);
 
-        if (gcmReqistration == null) {
-            logger.info("user is NOT registered.");
-            return new WhereStatus(UNKNOWN_USER, "unknown user: " + requesterEmail);
-        } else {
-            logger.info("user is registered, will set his/her status to " + (active?"active":"inactive"));
-            if (active) {
-                activeUsers.add(requesterEmail);
-            } else {
-                activeUsers.remove(requesterEmail);
-            }
-            return new WhereStatus("ok. status set to " + (active?"active":"inactive"));
-        }
-    }
+                        //TODO: update notification with the received location
+                        Message msg = Message.obtain();
+                        msg.what = GooglePlayServicesActivity.LOCATION_PUSH;
+                        msg.obj = extras;
 
-    @ApiMethod(name = "publish", httpMethod = "post")
-    public WhereStatus publishLocation(User user, WhereLocation location) throws ServiceException {
-        logger.info("user: " + user + " has location: " + location);
+                        WhereApplication.getContext().getWearHelper().showFoundNotification(WhereApplication.getContext().getWearHelper().findPerson, extras.getString("location", "unknown"));
 
-        if (user == null) {
-            throw new ServiceException(401, new NullPointerException("user"));
-        }
-
-        if (location == null) {
-            throw new ServiceException(400, new NullPointerException("location"));
-        }
-
-        int locationPushCount = 0;
-
-        String requesterEmail = formatEmail(user.getEmail());
-
-        Set<String> requesters = locationRequestsMap.remove(requesterEmail);
-        if (requesters == null) {
-            logger.info("Received unsolicited location update from " + requesterEmail);
-        } else {
-            Map<String, String> messageMap = new HashMap<>();
-            messageMap.put("action", "disclose");
-            messageMap.put("user", requesterEmail);
-            messageMap.put("location", location.getLocation());
-            messageMap.put("activity", location.getActivity());
-
-            for (String requester : requesters) {
-                if (!activeUsers.contains(requester)) {
-                    logger.info("Unable to deliver location update to " + requester + ": user is NOT active");
-                    continue;
-                }
-
-                GcmReqistration gcmReqistration = ofy().load().type(GcmReqistration.class).id(requester).now();
-//                String gcmId = gcmRegistrations.get(requester);
-//                if (gcmId == null) {
-                if (gcmReqistration == null) {
-                    logger.info("Unable to deliver location update to " + requester + ": GCM ID is unknown");
-                } else {
-                    try {
-                        String gcmId = gcmReqistration.gcmId;
-                        sendMessageToDevice(gcmId, messageMap);
-
-                        locationPushCount++;
-                    } catch (IOException e) {
-                        logger.log(Level.WARNING, "Cannot send GCM location-message to " + requester, e);
+                        WhereHelper.sendMessage(msg);
+                    } else {
+                        Log.i(TAG, "Unknown GCM message.");
                     }
                 }
+
+                /*// Post notification of received message.
+                sendNotification("Received: " + extras.toString());
+                WhereHelper.setTextStatus("GCM message: " + extras.toString());
+                Log.i(TAG, "Received GCM message: " + extras.toString());*/
             }
         }
-
-        return new WhereStatus("location received and successfully sent to " + locationPushCount +
-                " user" + (locationPushCount==1?"":"s"));
-    }
-
-    /* service status methods. */
-    public static int getLocationRequestsCount() {
-        int count = 0;
-
-        if (instance != null) {
-            count = instance.locationRequestsMap.size();
-        }
-
-        return count;
-    }
-
-    private Result sendMessageToDevice(String gcmTargetId, Map<String, String> messageMap) throws IOException {
-        Sender sender = new Sender(Constants.API_KEY);
-
-        Message.Builder message = new Message.Builder();
-        for (String key : messageMap.keySet()) {
-            String value = messageMap.get(key);
-            message.addData(key, value);
-        }
-
-        return sender.send(message.build(), gcmTargetId, 5);
-    }
-
-    /* remove dot from username and replace googlemail with gmail */
-    private String formatEmail(String email) {
-        int atPos = email.indexOf('@');
-        int dotPos = email.indexOf(".");
-        while (atPos != -1 && dotPos != -1 && dotPos < atPos) {
-            email = email.replaceFirst("\\.","");
-            dotPos = email.indexOf(".");
-        }
-        email = email.replaceAll("@googlemail.com", "@gmail.com");
-
-        return email.toLowerCase();
+        // Release the wake lock provided by the WakefulBroadcastReceiver.
+        GcmBroadcastReceiver.completeWakefulIntent(intent);
     }
 }
